f13ae14bdeaada1b42c5f2bd9c2e86d616f359f8
nataliia-v <nataly.verbenskaya@gmail.com>
2019-06-01 16:41:00 +0300

    wip

---
+++ src/App.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const morgan = require("morgan");
+
+class App {
+  constructor() {
+    this.app = express();
+  }
+
+  configure() {
+    this.app.use(bodyParser.urlencoded({ extended: true }));
+    this.app.use(bodyParser.json());
+
+    this.app.use(cors());
+
+    this.app.use(morgan("dev"));
+  }
+
+  start() {
+    this.app.listen(3003, () => {
+      console.log("Server is running. Use your API");
+    });
+  }
+}
+
+module.exports = App;
