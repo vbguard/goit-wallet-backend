947f87b631fd793bcb58e1d510ca580b07bf9f31
nataliia-v <nataly.verbenskaya@gmail.com>
2019-06-05 15:26:17 +0300

    post info about onu user

--- src/api/wallet/handlers/getAll.js
+++ src/api/wallet/handlers/getAll.js
@@ -1,8 +1,23 @@
-const Transaction = require('src/models/transaction');
+const Wallet = require('src/models/wallet');
 
 module.exports = async (req, res) => {
-  const items = await Transaction.find();
+  const userId = req.user._id;
+
+  const wallet = await Wallet.findOne({ createdBy: userId })
+    .populate('transactions')
+    .lean();
+
   return res.json({
-    items,
+    items: wallet.transactions,
   });
 };
+
+// //get allTransactions all users
+// const Transaction = require('src/models/transaction');
+//
+// module.exports = async (req, res) => {
+//   const items = await Transaction.find();
+//   return res.json({
+//     items,
+//   });
+// };
