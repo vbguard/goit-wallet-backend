c2799aad45f0989ba8d71124d234aa61fd1032c0
nataliia-v <nataly.verbenskaya@gmail.com>
2019-06-08 02:28:27 +0300

    fixed validation

--- src/validators/transactions/index.js
+++ src/validators/transactions/index.js
@@ -1,18 +1,20 @@
-// const Joi = require('src/validators/customJoi');
-// const Transaction = require('src/models/transaction');
-//
-// module.exports = {
-//   '/wallets/transactions': {
-//     POST: Joi.object({
-//       date: Joi.date().required(),
-//       type: Joi.string()
-//         .valid(Object.values(Transaction.TYPES))
-//         .required(),
-//       category: Joi.string()
-//         .valid(Object.values(Transaction.CATEGORIES))
-//         .required(),
-//       amount: Joi.number().required(),
-//       comments: Joi.string(),
-//     }),
-//   },
-// };
+const Joi = require('src/validators/customJoi');
+const Transaction = require('src/models/transaction');
+
+module.exports = {
+  '/wallets/transactions': {
+    POST: Joi.object({
+      date: Joi.date().required(),
+      type: Joi.string()
+        .valid(Object.values(Transaction.TYPES))
+        .required(),
+      category: Joi.string()
+        .valid(Object.values(Transaction.CATEGORIES))
+        .required(),
+      amount: Joi.number().required(),
+      comments: Joi.string()
+        .valid('')
+        .optional(),
+    }),
+  },
+};
