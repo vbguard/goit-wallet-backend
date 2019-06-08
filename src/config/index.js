339b7c6e7615fa4380908928a75dfee7df08fe92
nataliia-v <nataly.verbenskaya@gmail.com>
2019-06-08 14:23:59 +0300

    test

--- src/config/index.js
+++ src/config/index.js
@@ -3,5 +3,5 @@ module.exports = {
   ACCESS_TOKEN_EXPIRES_IN: '7h',
   PORT: 3003,
   MONGO_CONNECTION_URL:
-    'mongodb+srv://Supervisor:Masterkey@cluster0-mq8cq.gcp.mongodb.net/test?retryWrites=true&w=majority',
+    'mongodb+srv://admin:hudive87@cluster0-jdu25.gcp.mongodb.net/test?retryWrites=true&w=majority',
 };
